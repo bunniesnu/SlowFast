4aa285f1caaefdc287d4931c2564d44c6ffd2f4c
Bunniesnu <bunniesnu@gmail.com>
2025-07-15 17:35:55 +0900

    fix pip dependencies

--- install.sh
+++ install.sh
@@ -13,19 +13,20 @@ env_name=$(echo $(head -n 1 $conda_yml_file) | awk '{print $NF}')
 
 if conda env list | grep -qE "^\s*$env_name\s"; then
   echo "Environment '$env_name' already exists. Remove it, or edit the conda.yml file to change the conda environment name."
+  echo "To remove the environment, run:"
+  echo "conda env remove -n $env_name -y"
   exit 1
 fi
 
 conda env create -f $conda_yml_file
 conda activate $env_name
 
+pip install -r requirements.txt
+
 python -c "import torch; print('PyTorch Version:', torch.__version__); print('CUDA Available:', torch.cuda.is_available())"
 python -c "import torchvision; print('Torchvision Version:', torchvision.__version__)"
 python -c "import pytorchvideo; print('PyTorchVideo Version:', pytorchvideo.__version__)"
 gcc --version
-rm -rf $detectron2_repo_dir
-git clone https://github.com/facebookresearch/detectron2 $detectron2_repo_dir
-pip install -e $detectron2_repo_dir
 
 echo "Installing SlowFast"
 export PYTHONPATH="$(pwd)/slowfast:$PYTHONPATH"
